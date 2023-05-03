ec02d69dcd5e84b47990659af6ddef326bb94e69
Andres Gonzalez <gonche1124@aol.com>
2023-05-02 22:47:26 -0700

    - minor requirements

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GoncheLibrary",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v12),
         .iOS(.v16),
         .tvOS(.v16),
         .watchOS(.v7)
